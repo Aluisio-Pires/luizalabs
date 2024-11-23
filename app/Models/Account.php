a5b0f46df8d04b1193276b427b4e3bfa3c4f554f
Aluisio Pires <aluisiopireseng@gmail.com>
2024-11-23 04:59:32 -0300

    Model Account configurado

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -4,9 +4,50 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Account extends Model
 {
-    /** @use HasFactory<\Database\Factories\AccountFactory> */
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'number',
+        'balance',
+        'credit_limit',
+        'user_id',
+    ];
+
+    /**
+     * @return BelongsTo<User,$this>
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * @return HasMany<Transaction,$this>
+     */
+    public function transactions(): HasMany
+    {
+        return $this->hasMany(Transaction::class);
+    }
+
+    /**
+     * @return HasMany<Transaction,$this>
+     */
+    public function inflows(): HasMany
+    {
+        return $this->hasMany(Transaction::class, 'destination_account_id');
+    }
+
+    /**
+     * @return HasMany<Subledger,$this>
+     */
+    public function subledgers(): HasMany
+    {
+        return $this->hasMany(Subledger::class, 'account_id');
+    }
 }
