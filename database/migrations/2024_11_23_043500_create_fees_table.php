cb3e0793171da12319deab2ee554436830242240
Aluisio Pires <aluisiopireseng@gmail.com>
2024-11-23 04:15:41 -0300

    Adicionada a entidade Fee

---
+++ database/migrations/2024_11_23_043500_create_fees_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Models\TransactionType;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('fees', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->enum('type', ['fixed', 'percentage']);
+            $table->bigInteger('value')->default(0);//Valor em Microns
+
+            $table->foreignIdFor(TransactionType::class)->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('fees');
+    }
+};
