684bc021497c3fe5d879bbfbdc4f43e07255f446
Aluisio Pires <aluisiopireseng@gmail.com>
2024-11-25 08:59:59 -0300

    Criação das rotas de api

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreTransactionRequest;
 use App\Http\Requests\UpdateTransactionRequest;
+use App\Http\Services\TransactionService;
 use App\Models\Transaction;
 use Illuminate\Support\Facades\Gate;
 use Inertia\Inertia;
@@ -33,7 +34,9 @@ class TransactionController extends Controller
      */
     public function store(StoreTransactionRequest $request)
     {
-        dd($request->validated());
+        Gate::authorize('create', Transaction::class);
+        $service = new TransactionService;
+        dd($service->create($request->validated()));
     }
 
     /**
